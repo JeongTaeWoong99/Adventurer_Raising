d106c1abf0b1b1f2d176ba05d15c5f76e5b75ab3
JeongTaeWoong99 <111197095+JeongTaeWoong99@users.noreply.github.com>
2025-07-13 22:31:18 +0900

    버그 수정
    툴팁 버그 수정(좌우 이동)

--- Assets/DB(Old)/Manager/ToolTipManager.cs
+++ Assets/DB(Old)/Manager/ToolTipManager.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,47 +9,42 @@ public class ToolTipManager : MonoBehaviour
     // 툴팁 화면 밖으로 나가는걸 방지
     private RectTransform tooltipRect;
     private float         halfwidth;
-    
-    // 툴팁
-    public GameObject      tooltipObject;
-    public TextMeshProUGUI nameText;
+    private float         tooltipOffset = 10f; // 툴팁 오프셋 전역변수
 
     private void Awake()
     {
         instance = this;
-        
-        tooltipRect = tooltipObject.GetComponent<RectTransform>();
     }
 
     private void Start()
     {
-        tooltipObject.SetActive(false);
-        
-        halfwidth = GetComponent<CanvasScaler>().referenceResolution.x * 0.5f;    
+        tooltipRect = ClientManager.UI.manageUI.ToolTip.GetComponent<RectTransform>();
     }
 
     private void Update()
     {
         // 툴팁이 활성화되어 있을 때만 위치 업데이트
-        if (tooltipObject.activeInHierarchy)
+        if (ClientManager.UI.manageUI.ToolTip.activeInHierarchy)
         {
-            tooltipObject.transform.position = Input.mousePosition;
-
-            if (tooltipRect.anchoredPosition.x + tooltipRect.sizeDelta.x > halfwidth)
+            // 마우스 오른쪽에 툴팁이 나타날 때 화면 밖으로 나가는지 판단
+            if (Input.mousePosition.x + tooltipRect.sizeDelta.x + tooltipOffset > Screen.width)
             {
+                // 화면 밖으로 나가면 왼쪽으로 표시
                 tooltipRect.pivot = new Vector2(1, 0.5f);
-                tooltipObject.transform.position -= new Vector3(10, 0, 0);
+                ClientManager.UI.manageUI.ToolTip.transform.position = Input.mousePosition - new Vector3(tooltipOffset, 0, 0);
             }
             else
             {
+                // 기본: 마우스 오른쪽에 표시
                 tooltipRect.pivot = new Vector2(0, 0.5f);
-                tooltipObject.transform.position += new Vector3(10, 0, 0);
+                ClientManager.UI.manageUI.ToolTip.transform.position = Input.mousePosition + new Vector3(tooltipOffset, 0, 0);
             }
         }
     }
 
     public void SetupToolTip(string name)
     {
-        nameText.text = name;
+        ClientManager.UI.manageUI.ToolTipNameText.text = name;
+        Debug.Log("이름변경 =>" + name);
     }
 }
--- Assets/Scripts/Client/UI/Scene/Scene_Use_UI/UI_Manage.cs
+++ Assets/Scripts/Client/UI/Scene/Scene_Use_UI/UI_Manage.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class UI_Manage : UI_Scene
 {
 	[Header("GameObject")]
 	public GameObject LoadingPanelObject => GetObject((int)GameObjects.LoadingPanel);
+	public GameObject ToolTip			 => GetObject((int)GameObjects.ToolTip);
 		
+	[Header("TextMeshProUGUI")]
+	public TextMeshProUGUI ToolTipNameText   => GetText((int)Texts.ToolTipNameText);
+	
 	enum GameObjects
 	{
-		LoadingPanel
+		LoadingPanel, ToolTip
+	}
+	
+	enum Texts
+	{
+		ToolTipNameText,
 	}
 
 	public override void Init()
@@ -15,6 +25,11 @@ public class UI_Manage : UI_Scene
 		// 게임오브젝트 바인드
 		Bind<GameObject>(typeof(GameObjects));
 		
-		GetObject((int)GameObjects.LoadingPanel).gameObject.SetActive(true); // 로딩 패널 켜기		
+		GetObject((int)GameObjects.LoadingPanel).gameObject.SetActive(true); // 로딩 패널 켜기	
+		
+		GetObject((int)GameObjects.ToolTip).gameObject.SetActive(false);	 // 툴팁 끄기
+		
+		// 일반 텍스트
+		Bind<TextMeshProUGUI>(typeof(Texts));
 	}
 }
